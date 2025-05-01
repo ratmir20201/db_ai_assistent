0625e3b1afd097e044d289b58f9987ab052417f1
Ратмир Сарбасов <ratmir2020.1@gmail.com>
2025-05-01 13:55:40 +0500

    добавил front

---
+++ frontend/main.js
@@ -0,0 +1,66 @@
+function sendMessage() {
+  const chatMessages = document.getElementById("chatMessages");
+  const userInput = document.getElementById("userInput");
+
+  if (!userInput.value.trim()) return;
+
+  const userMsg = document.createElement("div");
+  userMsg.className = "message user-message";
+  userMsg.innerHTML = userInput.value;
+  chatMessages.append(userMsg)
+
+  userInput.value = "";
+
+  const thinkingMsg = document.createElement("div");
+  thinkingMsg.className = "message bot-message";
+  thinkingMsg.innerHTML = "⏳ Думаю...";
+
+  setTimeout(function() {
+    chatMessages.append(thinkingMsg);
+  }, 1200)
+
+  fetch("http://localhost:8000/ask", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({question: userInput.value})
+  })
+  .then(function(response) {return response.json();})
+  .then(data => {
+    thinkingMsg.remove();
+
+    const formatted_text = `
+    🧠 Объяснение:
+    ${data.explanation}
+
+    💡 SQL-запрос:
+    \`\`\`sql
+    ${data.sql_query}
+    \`\`\`
+
+    📊 Результат:
+    ${data.result.map(el => el.join(" | ")).join("\n")}
+    `;
+
+    typeBotResponse(formatted_text);
+  })
+  .catch(error => {
+    thinkingMsg.remove();
+    typeBotResponse("Произошла ошибка: ", error.message)
+  })
+
+}
+
+function typeBotResponse(text) {
+  const botMsg = document.createElement("div");
+    botMsg.className = "message bot-message";
+    chatMessages.append(botMsg);
+
+    let i = 0;
+    const interval = setInterval(function() {
+      botMsg.innerHTML += text[i];
+      i++;
+      if (i >= text.length) {
+        clearInterval(interval);
+      }
+    }, 30)
+}
