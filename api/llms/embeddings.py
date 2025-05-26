ac6c029bf9dbedc48b4eb4076192d67e1cf2aad8
test <ratmir2020.1@gmail.com>
2025-05-26 14:58:04 +0500

    изменил text_splitter

--- api/llms/embeddings.py
+++ api/llms/embeddings.py
@@ -1,12 +1,16 @@
-from langchain.text_splitter import CharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from db_parsing.vertica_parse import parse_vertica_to_documents
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(documents=parse_vertica_to_documents())
 
 local_embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
 vectorstore = Chroma.from_documents(documents=docs, embedding=local_embeddings)
+
+
+if __name__ == "__main__":
+    print(vectorstore.similarity_search("contract", 50))
