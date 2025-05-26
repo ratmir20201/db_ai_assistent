e96cd302fce5f9ca5c64a20a2162e245fdc0c321
Ратмир Сарбасов <ratmir2020.1@gmail.com>
2025-05-26 19:34:44 +0500

    создал импровизированную схему бд

---
+++ api/db_parsing/test_db_schema.py
@@ -0,0 +1,36 @@
+mock_data = [
+    {
+        "schema": "DM",
+        "table": "CLIENTS",
+        "table_comment": "",
+        "columns": [
+            {
+                "name": "client_id",
+                "type": "int",
+                "comment": "Идентификатор клиента",
+            },
+            {
+                "name": "client_name",
+                "type": "varchar",
+                "comment": "Имя клиента",
+            },
+        ],
+    },
+    {
+        "schema": "DM",
+        "table": "CONTRACT_INFO",
+        "table_comment": "",
+        "columns": [
+            {
+                "name": "contract_id",
+                "type": "int",
+                "comment": "Идентификатор контракта",
+            },
+            {
+                "name": "contract_date",
+                "type": "date",
+                "comment": "Дата контракта",
+            },
+        ],
+    },
+]
