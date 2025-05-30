24b776b27eb38dd99e207ab1f711b3309e241d74
test <ratmir2020.1@gmail.com>
2025-05-30 13:37:19 +0500

    добавил новый ip в cors

--- api/main.py
+++ api/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "http://192.168.99.140:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
